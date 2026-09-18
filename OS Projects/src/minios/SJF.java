2dbe838cfc8baa1987789290453d08afddfe64a5
Mingwei-Xu <mingwei.xu@bsu.edu>
2026-09-18 13:16:33 -0400

    Implement addProcess and selectNextProcess methods
    Implemented methods to add a process and select the next process based on the shortest duration in the SJF scheduling algorithm.

--- OS Projects/src/minios/SJF.java
+++ OS Projects/src/minios/SJF.java
@@ -1,14 +1,44 @@
+
 package minios;
 
 import java.util.List;
 
 public class SJF implements SchedulingAlgo {
+
+
+
     @Override
     public void addProcess(List<Process> readyQueue, Process p) {
+        readyQueue.add(p);
     }
 
     @Override
     public Process selectNextProcess(List<Process> readyQueue) {
-    }
 
+        if (readyQueue.isEmpty()) {
+            return null;
+        }
+
+        int shortestIndex = 0;
+        int shortestDuration = readyQueue.get(0).getCurrentInstruction().duration;
+
+        for (int i = 1; i < readyQueue.size(); i++) {
+            Process p = readyQueue.get(i);
+            Instruction inst = p.getCurrentInstruction();
+
+
+            if (inst == null) {
+                continue;
+            }
+
+            if (inst.duration < shortestDuration) {
+                shortestDuration = inst.duration;
+                shortestIndex = i;
+            }
+
+        }
+
+
+        return readyQueue.remove(shortestIndex);
+    }
 }
