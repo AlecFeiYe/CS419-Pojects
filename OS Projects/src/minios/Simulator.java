8bb8781a9f56200a775dba233217feb900bcb244
yefei <fei.ye@bsu.edu>
2026-09-25 20:18:13 +0000

    Give a MemorySize to Kernel.

--- OS Projects/src/minios/Simulator.java
+++ OS Projects/src/minios/Simulator.java
@@ -46,28 +46,28 @@ public class Simulator {
         System.out.println("----------  Running FCFS  ----------\n");
         List<Process> workloadFCFS = TraceParser.parseWorkload(filePath);
         SchedulingAlgo algoFCFS = new FCFS();
-        Kernel kernelFCFS = new Kernel(algoFCFS);
+        Kernel kernelFCFS = new Kernel(algoFCFS, 100);
         Simulator simFCFS = new Simulator(kernelFCFS, workloadFCFS);
         simFCFS.run();
-        System.out.println("Average Waiting Time: " + kernelFCFS.getAverageWaitingTime()+"\n");
+        System.out.println("Average Waiting Time: " + kernelFCFS.getAverageWaitingTime() + "\n");
 
         // 2. SJF 
         System.out.println("----------  Running SJF  ----------\n");
         List<Process> workloadSJF = TraceParser.parseWorkload(filePath);
         SchedulingAlgo algoSJF = new SJF();
-        Kernel kernelSJF = new Kernel(algoSJF);
+        Kernel kernelSJF = new Kernel(algoSJF, 100);
         Simulator simSJF = new Simulator(kernelSJF, workloadSJF);
         simSJF.run();
-        System.out.println("Average Waiting Time: " + kernelSJF.getAverageWaitingTime()+"\n");
+        System.out.println("Average Waiting Time: " + kernelSJF.getAverageWaitingTime() + "\n");
 
         // 3. RR 
         System.out.println("----------  Running RR  ----------\n");
         List<Process> workloadRR = TraceParser.parseWorkload(filePath);
         SchedulingAlgo algoRR = new RR();
-        Kernel kernelRR = new Kernel(algoRR);
+        Kernel kernelRR = new Kernel(algoRR, 100);
         Simulator simRR = new Simulator(kernelRR, workloadRR);
         simRR.run();
-        System.out.println("Average Waiting Time: " + kernelRR.getAverageWaitingTime()+"\n");
+        System.out.println("Average Waiting Time: " + kernelRR.getAverageWaitingTime() + "\n");
 
         System.out.println("----------  Conclusion and Comparing  ----------\n");
         System.out.println("FCFS Average Waiting Time: " + kernelFCFS.getAverageWaitingTime());
